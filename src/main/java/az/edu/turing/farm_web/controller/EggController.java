f7ad36ff3dcf3bf2709d67bfb6affc6de9dcb37e
abilov042 <shamilabilov599@gmail.com>
2024-07-15 17:37:41 +0400

    allow cross

--- src/main/java/az/edu/turing/farm_web/controller/EggController.java
+++ src/main/java/az/edu/turing/farm_web/controller/EggController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/eggs")
 @RequiredArgsConstructor
+@CrossOrigin
 public class EggController {
 
     private final EggService eggService;
