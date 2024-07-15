7dc464671189509e477e220a981c71d26e8ca7b9
abilov042 <shamilabilov599@gmail.com>
2024-07-15 17:35:31 +0400

    allow cross

--- src/main/java/az/edu/turing/farm_web/controller/ProductController.java
+++ src/main/java/az/edu/turing/farm_web/controller/ProductController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
+@CrossOrigin
 public class  ProductController {
 
     private final ProductService productService;
